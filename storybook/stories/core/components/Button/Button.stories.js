10ad83d94c6a979ff78d29c0c52d2e5b7e68a44c
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-01-12 22:17:09 +0530

    OHRM-945:Initiate OXD with Vue 2.x + Storybook

---
+++ storybook/stories/core/components/Button/Button.stories.js
@@ -0,0 +1,41 @@
+import Button from "@orangehrm/oxd/core/components/Button/Button.vue";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: "color" },
+    size: {
+      control: { type: "select", options: ["small", "medium", "large"] },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { "oxd-button": Button },
+  template: '<oxd-button @onClick="onClick" v-bind="$props" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: "Button",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Button",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  label: "Button",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  label: "Button",
+};
