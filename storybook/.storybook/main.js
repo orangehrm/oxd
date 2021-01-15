5e55e298bdb4df2a1d724fbcc2ff651e12bbeeb8
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-01-15 14:27:21 +0530

    OHRM-945:Add storybook sass support

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,7 +1,24 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-links/register",
+    "@storybook/addon-actions/register",
+    "@storybook/addon-viewport/register",
+  ],
+  // https://storybook.js.org/docs/react/configure/webpack#extending-storybooks-webpack-config
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../../"),
+    });
+
+    return config;
+  },
 };
