9da62416b47b0b36501349701eda210e7d65e08a
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-01-23 11:54:20 +0530

    OHRM-945:Implement button effects

--- components/src/core/components/Button/types.ts
+++ components/src/core/components/Button/types.ts
@@ -1,8 +1,19 @@
 export const SIZE_SMALL = 'small';
 export const SIZE_MEDIUM = 'medium';
 export const SIZE_LARGE = 'large';
+export const SIZES = [SIZE_SMALL, SIZE_MEDIUM, SIZE_LARGE];
+
+export const TYPE_MAIN = 'main';
+export const TYPE_SECONDARY = 'secondary';
+export const TYPE_GHOST = 'ghost';
+export const TYPES = [TYPE_MAIN, TYPE_SECONDARY, TYPE_GHOST];
 
 export type ButtonSize =
   | typeof SIZE_SMALL
   | typeof SIZE_MEDIUM
   | typeof SIZE_LARGE;
+
+export type ButtonType =
+  | typeof TYPE_MAIN
+  | typeof TYPE_SECONDARY
+  | typeof TYPE_GHOST;
