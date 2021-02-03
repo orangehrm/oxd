73b825503ba3b27893c36e00f89aba3a8b3a5c5d
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-02-03 13:21:24 +0530

    OHRM-1008:Add text & divider components

---
+++ components/src/core/components/Divider/types.ts
@@ -0,0 +1,6 @@
+export const AREA_ORIENTATION_VERTICAL = 'vertical';
+export const AREA_ORIENTATION_HORIZONTAL = 'horizontal';
+
+export type AreaOrientation =
+  | typeof AREA_ORIENTATION_VERTICAL
+  | typeof AREA_ORIENTATION_HORIZONTAL;
