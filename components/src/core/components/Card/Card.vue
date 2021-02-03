a42ed75d858bd9bce94fd4e1dc0afb5289c74194
Rajitha Kumara <rajitha@orangehrm.us.com>
2021-02-03 13:21:24 +0530

    OHRM-1008:Add text & divider components

---
+++ components/src/core/components/Card/Card.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    v-focus-within
+    class="oxd-card"
+    :class="[
+      clickable && 'oxd-card--clickable',
+      hasShadow && 'oxd-card--hasShadow',
+    ]"
+    @click="cardClick"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    clickable: {
+      type: Boolean,
+    },
+    hasShadow: {
+      type: Boolean,
+    },
+  },
+  methods: {
+    cardClick() {
+      if (!this.clickable) return;
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.oxd-card {
+  display: flex;
+  border: 1px solid var(--card-border-color);
+  border-radius: var(--card-border-radius);
+  outline: none;
+
+  &--noBorder {
+    border: none;
+    &:focus,
+    &:hover {
+      border: none;
+    }
+  }
+
+  &--hasHoverShadow:focus,
+  &--hasHoverShadow:hover {
+    box-shadow: var(--shadow-2);
+  }
+
+  &--hasShadow {
+    box-shadow: var(--shadow-2);
+  }
+
+  &--clickable:hover,
+  &--clickable:focus {
+    box-shadow: var(--shadow-2);
+    cursor: pointer;
+  }
+}
+</style>
---
+++ components/src/core/components/Divider/Divider.vue
@@ -0,0 +1,36 @@
+<template>
+  <hr
+    :class="classes"
+    :style="style"
+    role="separator"
+    :aria-orientation="areaOrientation"
+  />
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {AREA_ORIENTATION_HORIZONTAL, AreaOrientation} from './types';
+
+export default defineComponent({
+  name: 'oxd-divider',
+
+  props: {
+    style: {
+      type: Object,
+    },
+  },
+
+  computed: {
+    classes(): object {
+      return {
+        'oxd-divider': true,
+      };
+    },
+    areaOrientation(): AreaOrientation {
+      return AREA_ORIENTATION_HORIZONTAL;
+    },
+  },
+});
+</script>
+
+<style src="./divider.scss" lang="scss" scoped></style>
