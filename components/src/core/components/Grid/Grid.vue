a62bcc5c3b56f656df52f6d3f98485706c7fb275
Samantha Jayasinghe <samantha.jayasinghe@totaralearning.com>
2021-03-02 17:53:43 +1300

    Add grid component

---
+++ components/src/core/components/Grid/Grid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="oxd-grid">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'oxd-grid',
+
+  props: {
+    rows: {
+      type: Number,
+    },
+    cols: {
+      type: Number,
+    },
+    gap: {
+      type: Number,
+    },
+  },
+});
+</script>
+
+<style src="./grid.scss" lang="scss" scoped></style>
