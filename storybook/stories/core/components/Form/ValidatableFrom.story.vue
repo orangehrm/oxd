5ff6ed2309ad48605e6f0bcf5bf14846d254ebd3
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-03-15 08:43:20 +0530

    form validation story added

---
+++ storybook/stories/core/components/Form/ValidatableFrom.story.vue
@@ -0,0 +1,101 @@
+<template>
+  <oxd-text tag="h5">Add Job Category</oxd-text>
+
+  <oxd-divider />
+
+  <oxd-form @submit="checkForm" @submitValid="getFormValues" ref="form">
+    <oxd-form-row>
+      <oxd-form-group class="orangehrm-bottom-space">
+        <oxd-input-field
+            label="Job Category Name"
+            v-model="name"
+            :rules="rules.name"
+          />
+          <oxd-input-field
+            label="Job Category Id"
+            v-model="id"
+            :rules="rules.id"
+          />
+      </oxd-form-group>
+    </oxd-form-row>
+
+    <oxd-divider />
+
+    <oxd-form-actions>
+      <oxd-button displayType="ghost" label="Cancel" />
+      <oxd-button
+        class="orangehrm-left-space"
+        displayType="secondary"
+        label="Add"
+        type="submit"
+      />
+    </oxd-form-actions>
+
+  </oxd-form>
+
+  <p>Form is {{ isValid ? 'vaild' : 'invalid' }}</p>
+
+</template>
+
+<script>
+import Form from '@orangehrm/oxd/core/components/Form/Form';
+import FormRow from '@orangehrm/oxd/core/components/Form/FormRow';
+import FormGroup from '@orangehrm/oxd/core/components/Form/FormGroup';
+import FormActions from '@orangehrm/oxd/core/components/Form/FormActions';
+import InputField from "@orangehrm/oxd/core/components/InputField/InputField";
+import Divider from '@orangehrm/oxd/core/components/Divider/Divider';
+import Button from '@orangehrm/oxd/core/components/Button/Button';
+import Text from '@orangehrm/oxd/core/components/Text/Text';
+
+export default {
+  name: 'ValidatableFrom',
+
+  data() {
+    return {
+      name: "",
+      id: "",
+      rules: {
+        name: [
+          v => (!!v && v.trim() !== "") || "Required",
+          v => (v && v.length <= 50) || "Should be less than 50 characters"
+        ],
+        id: [
+          v => (!!v && v.trim() !== "") || "Required",
+          v => (v && v.length >= 10) || "Should more than 10 characters"
+        ]
+      },
+      isValid: true
+    };
+  },
+
+  components: {
+    'oxd-form': Form,
+    'oxd-form-row': FormRow,
+    'oxd-form-group': FormGroup,
+    'oxd-form-actions': FormActions,
+    'oxd-input-field': InputField,
+    'oxd-divider': Divider,
+    'oxd-button': Button,
+    'oxd-text': Text,
+  },
+
+  methods: {
+    getFormValues() {
+      console.log('form submit');
+    },
+    checkForm() {
+      this.isValid = this.$refs.form.isValid;
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.orangehrm-left-space {
+  margin-left: 10px;
+}
+
+.orangehrm-bottom-space {
+  margin-bottom: 10px;
+}
+</style>
