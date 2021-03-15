187df6a87b53d9e1f4668ffc1ca7836f250b891a
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-03-15 08:42:20 +0530

    oxd-button type prop changed

--- components/src/core/components/Button/Button.vue
+++ components/src/core/components/Button/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" :class="classes" :style="style" @click="onClick">
+  <button :type="type" :class="classes" :style="style">
     <slot name="icon">
       <oxd-icon v-if="iconName" :name="iconName" class="oxd-button-icon" />
     </slot>
@@ -40,15 +40,19 @@ export default defineComponent({
     },
     type: {
       type: String,
+      default: 'button',
+    },
+    displayType: {
+      type: String,
       default: TYPE_MAIN,
-      validator: function(value: ButtonType) {
+      validator: function (value: ButtonType) {
         return TYPES.indexOf(value) !== -1;
       },
     },
     size: {
       type: String,
       default: SIZE_MEDIUM,
-      validator: function(value: ButtonSize) {
+      validator: function (value: ButtonSize) {
         return SIZES.indexOf(value) !== -1;
       },
     },
@@ -71,16 +75,8 @@ export default defineComponent({
       return {
         'oxd-button': true,
         [`oxd-button--${this.size}`]: true,
-        [`oxd-button--${this.type}`]: true,
+        [`oxd-button--${this.displayType}`]: true,
       };
-    },
-  },
-
-  methods: {
-    onClick(e: Event) {
-      e.stopPropagation();
-      e.preventDefault();
-      this.$emit('click', e);
     },
   },
 });
