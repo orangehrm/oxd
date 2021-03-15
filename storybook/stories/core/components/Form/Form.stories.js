320f83a42101dda49dbc1486f3588c953af93632
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-03-15 22:17:18 +0530

    Dynamic form validation story added

--- storybook/stories/core/components/Form/Form.stories.js
+++ storybook/stories/core/components/Form/Form.stories.js
@@ -2,6 +2,7 @@ import DefaultForm from './DefaultForm.story.vue';
 import AddJobTitle from './AddJobTitle.story.vue';
 import CustomLabelForm from './CustomLabelForm.story.vue';
 import ValidatableFrom from './ValidatableFrom.story.vue';
+import DynamicValidatableFrom from './DynamicValidatableFrom.story.vue';
 
 export default {
   title: 'Example/Form',
@@ -14,3 +15,5 @@ export const CustomLabel = () => CustomLabelForm;
 export const AddJobTitleForm = () => AddJobTitle;
 
 export const FormWithValidation = () => ValidatableFrom;
+
+export const DynamicFormWithValidation = () => DynamicValidatableFrom;
