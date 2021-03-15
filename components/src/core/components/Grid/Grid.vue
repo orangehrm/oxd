d09fa95885a60bc7a4d13ea868cb9c698cd672a4
Samantha Jayasinghe <isamantha@gmail.com>
2021-03-15 23:35:56 +1300

    OHRM-1070:Develop grid component (#16)

---
+++ components/src/core/components/Grid/Grid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="className">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'oxd-grid',
+
+  props: {
+    cols: {
+      type: Number,
+    },
+  },
+
+  computed: {
+    className(): String {
+      return `oxd-grid-${this.cols}`;
+    },
+  },
+});
+</script>
+
+<style src="./grid.scss" lang="scss" scoped></style>
---
+++ components/src/core/components/Grid/GridItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="oxd-grid-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'oxd-grid-item',
+});
+</script>
+
+<style src="./griditem.scss" lang="scss" scoped></style>
