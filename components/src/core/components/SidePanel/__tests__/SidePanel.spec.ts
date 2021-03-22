abe793bae8c5c7fc2b43d652f139426e43eed693
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-03-23 01:22:01 +0530

    oxd sidepanel component unit tests added

---
+++ components/src/core/components/SidePanel/__tests__/SidePanel.spec.ts
@@ -0,0 +1,59 @@
+import {mount, shallowMount} from '@vue/test-utils';
+import SidePanel from '@orangehrm/oxd/core/components/SidePanel/SidePanel.vue';
+import MainMenu from '@orangehrm/oxd/core/components/SidePanel/MainMenu.vue';
+import MainMenuItem from '@orangehrm/oxd/core/components/SidePanel/MainMenuItem.vue';
+
+describe('SidePanel.vue', () => {
+  it('renders OXD Side Panel', () => {
+    const wrapper = shallowMount(SidePanel);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should render OXD Side Panel with menu items', async () => {
+    const menuItems = [
+      {
+        name: 'Home',
+        url: '#',
+        icon: 'icon-home',
+        active: false,
+      },
+      {
+        name: 'PIM',
+        url: '#',
+        icon: 'icon-pim',
+        active: true,
+      },
+      {
+        name: 'Time',
+        url: '#',
+        icon: 'icon-time',
+        active: false,
+      },
+    ];
+    const wrapper = mount(SidePanel, {
+      props: {menuItems},
+    });
+    const menuElems = wrapper.findAllComponents(MainMenuItem);
+    expect(menuElems.length).toBe(3);
+    expect(menuElems[0].findAll('a')[1].classes()).toContain('active');
+  });
+
+  it('should emit OXD Side Panel collapse event', () => {
+    const menuItems = [
+      {
+        name: 'Home',
+        url: '#',
+        icon: 'icon-home',
+        active: false,
+      },
+    ];
+    const wrapper = mount(SidePanel, {
+      props: {menuItems},
+    });
+    wrapper
+      .findComponent(MainMenu)
+      .find('.oxd-main-menu-button')
+      .trigger('click');
+    expect(wrapper.emitted('collapse')).toBeTruthy();
+  });
+});
