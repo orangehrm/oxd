7521e8089c74afaaa312aed2a24a9c350135a0d9
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-03-31 10:48:27 +0530

    stylesheets renamed to kebab case

---
+++ components/src/core/components/SidePanel/MainMenuItem.vue
@@ -0,0 +1,74 @@
+<template>
+  <li class="oxd-main-menu-item-wrapper">
+    <a :class="menuClasses" :href="url">
+      <i :class="iconClasses"></i>
+      <oxd-text
+        tag="span"
+        :class="{'oxd-main-menu-item--name': true, active: active}"
+        >{{ name }}</oxd-text
+      >
+    </a>
+  </li>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import Text from '@orangehrm/oxd/core/components/Text/Text.vue';
+
+export default defineComponent({
+  name: 'oxd-main-menu-item',
+
+  props: {
+    url: {
+      type: String,
+      default: '#',
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    collapsed: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: String,
+      default: 'icon-home',
+    },
+  },
+
+  components: {
+    'oxd-text': Text,
+  },
+
+  computed: {
+    iconClasses(): object {
+      return {
+        'oxd-menu-icon': true,
+        [this.icon]: !this.active,
+        [`${this.icon}-w`]: this.active,
+      };
+    },
+    menuClasses(): object {
+      return {
+        'oxd-main-menu-item': true,
+        active: this.active,
+        toggle: this.collapsed,
+      };
+    },
+  },
+});
+</script>
+
+<style src="./main-menu.scss" lang="scss" scoped></style>
+<style lang="scss">
+@import '../../../styles/colors';
+.oxd-main-menu-button i {
+  color: $oxd-white-color !important;
+  font-size: 10px;
+}
+</style>
---
+++ components/src/core/components/Topbar/NotificationDropdown.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- Notification dropdown -->
+  <oxd-dropdown-menu class="oxd-notifdropdown">
+    <span class="oxd-notifdropdown-tab">
+      <oxd-icon
+        class="oxd-notifdropdown-icon"
+        name="bell-fill"
+        :with-container="false"
+      />
+      <span class="oxd-notifdropdown-count">30</span>
+    </span>
+    <template v-slot:content>
+      <!-- Notification items -->
+      <span class="oxd-notifdropdown-head">
+        Alerts Center
+      </span>
+      <hr class="oxd-notifdropdown-divider" />
+      <slot></slot>
+      <hr class="oxd-notifdropdown-divider" />
+      <li role="none">
+        <a href="#" role="menuitem" class="oxd-notifdropdown-link --center">
+          Show All Notifications
+        </a>
+      </li>
+    </template>
+  </oxd-dropdown-menu>
+  <!-- Notification dropdown -->
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import Icon from '@orangehrm/oxd/core/components/Icon/Icon.vue';
+import DropdownMenu from '@orangehrm/oxd/core/components/DropdownMenu/DropdownMenu.vue';
+
+export default defineComponent({
+  name: 'oxd-notification-dropdown',
+  components: {
+    'oxd-icon': Icon,
+    'oxd-dropdown-menu': DropdownMenu,
+  },
+});
+</script>
+
+<style src="./notification-dropdown.scss" lang="scss" scoped></style>
+<style lang="scss">
+.oxd-notifdropdown {
+  & .oxd-dropdown-menu {
+    right: 0;
+    left: unset;
+    top: 50px;
+    min-width: 10rem;
+  }
+}
+</style>
