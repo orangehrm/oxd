e8de3dc10ae671ef9997de939a326b95e85bcd38
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-04-09 11:53:16 +0530

    card table sort added, unit test updated

--- components/src/core/components/CardTable/TableHeaderCell.vue
+++ components/src/core/components/CardTable/TableHeaderCell.vue
@@ -1,14 +1,59 @@
 <template>
   <div class="oxd-table-header-cell" role="columnheader">
     <slot></slot>
+    <oxd-icon-button
+      v-if="sort"
+      :withContainer="false"
+      :name="sortIcon"
+      class="oxd-table-header-cell-sorticon"
+      @click="$emit('sort', sort)"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, PropType} from 'vue';
+import {Sort} from './types';
+import IconButton from '@orangehrm/oxd/core/components/Button/Icon.vue';
 
 export default defineComponent({
   name: 'oxd-card-th',
+
+  props: {
+    sort: {
+      type: Object as PropType<Sort>,
+      required: false,
+    },
+  },
+
+  emits: ['sort'],
+
+  components: {
+    'oxd-icon-button': IconButton,
+  },
+
+  computed: {
+    sortIcon(): string {
+      let icon = '';
+      if (this.sort) {
+        switch (this.sort.state) {
+          case 'asc':
+            icon = 'sort-alpha-up';
+            break;
+          case 'desc':
+            icon = 'sort-alpha-down';
+            break;
+          case 'none':
+            icon = '';
+            break;
+          default:
+            icon = 'arrow-down-up';
+            break;
+        }
+      }
+      return icon;
+    },
+  },
 });
 </script>
 
