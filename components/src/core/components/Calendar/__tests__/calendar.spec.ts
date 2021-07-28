1ff196b75ade6a79255905fe7777d71f94e8fec4
Chamara Abesinghe <chamara@orangehrmlive.com>
2021-07-28 13:26:59 +0530

    OHRM5X-432:oxd-calendar, oxd-date input unit tests

---
+++ components/src/core/components/Calendar/__tests__/calendar.spec.ts
@@ -0,0 +1,107 @@
+import {mount} from '@vue/test-utils';
+import Calendar from '@orangehrm/oxd/core/components/Calendar/Calendar.vue';
+import DateVue from '@orangehrm/oxd/core/components/Calendar/Date.vue';
+import Icon from '@orangehrm/oxd/core/components/Button/Icon.vue';
+import {nextSunday, nextSaturday, freshDate} from '../../../../utils/date';
+
+describe('Calendar.vue', () => {
+  const dateExpected = new Date(1990, 6, 27, 0, 0, 0);
+  it('renders OXD Calendar', () => {
+    const wrapper = mount(Calendar, {
+      props: {
+        modelValue: dateExpected,
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('should select date on click', async () => {
+    const wrapper = mount(Calendar, {});
+    const date = wrapper.findComponent(DateVue);
+    await date.trigger('click');
+    expect(wrapper.emitted('update:modelValue')).toBeTruthy();
+    expect(date.find('.oxd-calendar-date.--selected').exists()).toBeTruthy();
+  });
+  it('should show today date', async () => {
+    const wrapper = mount(Calendar, {});
+    const date = wrapper.findAllComponents(DateVue);
+    const today = freshDate();
+    expect(
+      date[today.getDate()].find('.oxd-calendar-date.--today').exists(),
+    ).toBeTruthy();
+  });
+  it('should show attributes in calendar', async () => {
+    const wrapper = mount(Calendar, {
+      props: {
+        modelValue: dateExpected,
+        dayAttributes: [
+          {
+            index: 0, // sunday
+            class: '--non-working-day',
+            style: {},
+          },
+          {
+            index: 6, // saturday
+            class: '--working-day-half',
+            style: {},
+          },
+        ],
+      },
+    });
+    const date = wrapper.findAllComponents(DateVue);
+    const nonWorkingDay = nextSunday(dateExpected);
+    const halfWorkingDay = nextSaturday(dateExpected);
+    expect(
+      date[nonWorkingDay.getDate()]
+        .find('.oxd-calendar-date-wrapper.--non-working-day')
+        .exists(),
+    ).toBeTruthy();
+    expect(
+      date[halfWorkingDay.getDate()]
+        .find('.oxd-calendar-date-wrapper.--working-day-half')
+        .exists(),
+    ).toBeTruthy();
+  });
+  it('should show events in calendar', async () => {
+    const event1 = {
+      date: nextSunday(dateExpected),
+      type: 'holiday',
+      class: '--holiday-full',
+    };
+    const event2 = {
+      date: new Date(1990, 6, 5, 0, 0, 0),
+      type: 'holiday',
+      class: '--holiday-half',
+    };
+    const wrapper = mount(Calendar, {
+      props: {
+        modelValue: dateExpected,
+        events: [event1, event2],
+      },
+    });
+    const date = wrapper.findAllComponents(DateVue);
+    expect(
+      date[event1.date.getDate()]
+        .find('.oxd-calendar-date.--holiday-full')
+        .exists(),
+    ).toBeTruthy();
+    expect(
+      date[event2.date.getDate()]
+        .find('.oxd-calendar-date.--holiday-half')
+        .exists(),
+    ).toBeTruthy();
+  });
+  it('should emit selectMonth on month change', async () => {
+    const wrapper = mount(Calendar, {});
+    await wrapper.findComponent(Icon).trigger('click');
+    expect(wrapper.emitted('selectMonth')).toBeTruthy();
+  });
+  it('should emit selectYear on year change', async () => {
+    const wrapper = mount(Calendar, {
+      props: {
+        modelValue: new Date(1990, 0, 1, 0, 0, 0),
+      },
+    });
+    await wrapper.findComponent(Icon).trigger('click');
+    expect(wrapper.emitted('selectYear')).toBeTruthy();
+  });
+});
---
+++ components/src/core/components/Input/__tests__/date-input.spec.ts
@@ -0,0 +1,69 @@
+import {mount} from '@vue/test-utils';
+import DateInput from '@orangehrm/oxd/core/components/Input/DateInput.vue';
+import Input from '@orangehrm/oxd/core/components/Input/Input.vue';
+import {formatDate, freshDate} from '../../../../utils/date';
+
+describe('DateInput.vue', () => {
+  it('renders OXD Date Input', () => {
+    const wrapper = mount(DateInput, {});
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('should open datepicker on click', async () => {
+    const wrapper = mount(DateInput, {});
+    wrapper.findComponent(Input).trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.open).toBeTruthy();
+    expect(wrapper.emitted('dateselect:opened')).toBeTruthy();
+    expect(wrapper.find('.oxd-calendar-wrapper').exists()).toBeTruthy();
+  });
+  it('should not open datepicker on click, if disabled', async () => {
+    const wrapper = mount(DateInput, {
+      props: {disabled: true},
+    });
+    wrapper.findComponent(Input).trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.open).toBeFalsy();
+    expect(wrapper.emitted('dateselect:opened')).toBeFalsy();
+    expect(wrapper.find('.oxd-calendar-wrapper').exists()).toBeFalsy();
+  });
+  it('should accept valid input', async () => {
+    const wrapper = mount(DateInput, {});
+    const input = wrapper.findComponent(Input);
+    (input.element as HTMLInputElement).value = '2021-07-30';
+    await input.trigger('input');
+    await input.trigger('blur');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted('update:modelValue')).toEqual([['2021-07-30']]);
+  });
+  it('should not accept invalid input', async () => {
+    const wrapper = mount(DateInput, {});
+    const input = wrapper.findComponent(Input);
+    (input.element as HTMLInputElement).value = '2021-15-30';
+    await input.trigger('input');
+    await input.trigger('blur');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted('update:modelValue')).toEqual([['']]);
+  });
+  it('should return today date onclick today', async () => {
+    const wrapper = mount(DateInput, {});
+    await wrapper.findComponent(Input).trigger('click');
+    await wrapper.vm.$nextTick();
+    await wrapper.find('.oxd-date-input-link.--today').trigger('click');
+    const dateExpected = formatDate(freshDate(), 'yyyy-MM-dd');
+    expect(wrapper.emitted('update:modelValue')).toEqual([[dateExpected]]);
+  });
+  it('should clear date onclick clear', async () => {
+    const wrapper = mount(DateInput, {});
+    await wrapper.findComponent(Input).trigger('click');
+    await wrapper.vm.$nextTick();
+    await wrapper.find('.oxd-date-input-link.--clear').trigger('click');
+    expect(wrapper.emitted('update:modelValue')).toEqual([['']]);
+  });
+  it('should close datepicker onclick close', async () => {
+    const wrapper = mount(DateInput, {});
+    await wrapper.findComponent(Input).trigger('click');
+    await wrapper.vm.$nextTick();
+    await wrapper.find('.oxd-date-input-link.--close').trigger('click');
+    expect(wrapper.emitted('dateselect:closed')).toBeTruthy();
+  });
+});
