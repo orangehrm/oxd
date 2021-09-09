a78799856f95ec5434847a0db91690e90a74c7e4
thilinasan16 <35391124+thilinasan16@users.noreply.github.com>
2021-09-09 19:59:20 +0530

    Add profile picture component
    This commit contains the implementation for Profile Picture Component.

---
+++ components/src/core/components/ProfilePic/ProfilePic.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header-image">
+    <slot> </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+export default defineComponent({
+  name: 'oxd-profile-pic',
+});
+</script>
+<style lang="scss" scoped>
+.header-image::v-deep img {
+  border-radius: 50%;
+  width: 3.75rem;
+  height: auto;
+  max-height: 3.75rem;
+  cursor: pointer;
+}
+</style>
---
+++ storybook/stories/core/components/ProfilePic/CustomTemplate.vue
@@ -0,0 +1,21 @@
+<template>
+  <oxd-profile-pix>
+    <img :src="data.imageSrc" />
+  </oxd-profile-pix>
+</template>
+
+<script>
+import ProfilePic from '@orangehrm/oxd/core/components/ProfilePic/ProfilePic';
+export default {
+  components: {
+    'oxd-profile-pix': ProfilePic,
+  },
+  data() {
+    return {
+      data: {
+        imageSrc: require('@orangehrm/oxd/assets/images/user-default-400.png'),
+      },
+    };
+  },
+};
+</script>
