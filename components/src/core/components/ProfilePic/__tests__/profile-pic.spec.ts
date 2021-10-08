a36108c3f6b25bc778abe9ac9149c8f5d7d98f02
Thilina Sumithrarachchi <thilinasan166@gmail.com>
2021-10-08 15:36:50 +0530

    Profile component update and icon refrences update

--- components/src/core/components/ProfilePic/__tests__/profile-pic.spec.ts
+++ components/src/core/components/ProfilePic/__tests__/profile-pic.spec.ts
@@ -1,17 +1,26 @@
 import {mount} from '@vue/test-utils';
 import ProfilePic from '@orangehrm/oxd/core/components/ProfilePic/ProfilePic.vue';
+import {SIZE_SMALL} from '@orangehrm/oxd/core/components/ProfilePic/types';
 
 describe('ProfilePic.vue', () => {
-  it('renders OXD ProfilePic', () => {
-    const wrapper = mount(ProfilePic, {});
-    expect(wrapper.html()).toMatchSnapshot();
-  });
   it('renders OXD ProfilePic with image', () => {
     const wrapper = mount(ProfilePic, {
       slots: {
         default: `<img/>`,
       },
     });
-    expect(wrapper.find('img').exists()).toBe(true);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('renders OXD ProfilePic with small image', () => {
+    const wrapper = mount(ProfilePic, {
+      props: {
+        displayType: SIZE_SMALL,
+      },
+      slots: {
+        default: `<img/>`,
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
   });
 });
---
+++ components/src/core/components/ProfilePic/types.ts
@@ -0,0 +1,6 @@
+export const SIZE_SMALL = 'small';
+export const SIZE_MEDIUM = 'medium';
+
+export const SIZES = [SIZE_SMALL, SIZE_MEDIUM];
+
+export type ImageSize = typeof SIZE_SMALL | typeof SIZE_MEDIUM;
