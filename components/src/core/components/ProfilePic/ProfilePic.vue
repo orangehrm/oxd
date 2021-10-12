c178e3fb10967837fb41ac569dfda4d9f4c42f9d
thilinasan16 <35391124+thilinasan16@users.noreply.github.com>
2021-10-12 19:53:56 +0530

    Support multiple sizes for profile picture

--- components/src/core/components/ProfilePic/ProfilePic.vue
+++ components/src/core/components/ProfilePic/ProfilePic.vue
@@ -1,21 +1,47 @@
 <template>
-  <div class="header-image">
+  <div :class="classes">
     <slot> </slot>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
+import {SIZES, SIZE_MEDIUM, ImageSize} from './types';
 export default defineComponent({
   name: 'oxd-profile-pic',
+  props: {
+    size: {
+      type: String,
+      default: SIZE_MEDIUM,
+      validator: function(value: ImageSize) {
+        return SIZES.indexOf(value) !== -1;
+      },
+    },
+  },
+  computed: {
+    classes(): object {
+      return {
+        'header-image': true,
+        [`profile-image--${this.size}`]: true,
+      };
+    },
+  },
 });
 </script>
 <style lang="scss" scoped>
-.header-image::v-deep img {
+.header-image :deep(img) {
   border-radius: 50%;
-  width: 3.15rem;
   height: auto;
-  max-height: 3.15rem;
   cursor: pointer;
+}
+
+.header-image.profile-image--medium :deep(img) {
+  width: 3.15rem;
+  max-height: 3.15rem;
+}
+
+.header-image.profile-image--small :deep(img) {
+  width: 2.8rem;
+  max-height: 2.8rem;
 }
 </style>
