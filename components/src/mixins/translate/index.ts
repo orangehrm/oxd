8d2494154de55c5244ba01e7ec865c3a8e4ee048
Yasiru <yasiru@orangehrmlive.com>
2022-01-11 20:52:58 +0530

    Add Localization Support for Select Component and Auto Complete Component

--- components/src/mixins/translate/index.ts
+++ components/src/mixins/translate/index.ts
@@ -4,10 +4,10 @@ export const translateMixin = defineComponent({
   methods: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     $vt(key: string) {
-      return this.$t(key);
-    },
-    $t(key: string) {
-      return key;
+      if (typeof this?.$t === 'function') {
+        return this.$t(key);
+      }
+      return 'key';
     },
   },
 });
