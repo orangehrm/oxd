086a768aae666ab1c162bfae841b693ed297db3a
Gayan Sandamal <gayansandamal@orangehrmlive.com>
2022-01-19 14:56:21 +0530

    resolved conflicts

--- components/src/core/components/Input/Select/SelectInput.vue
+++ components/src/core/components/Input/Select/SelectInput.vue
@@ -27,7 +27,7 @@
         v-if="showEmptySelector && !hideDropdownDefaultLabel"
         @select="onClear"
       >
-        {{ placeholder }}
+        {{ $vt(placeholder) }}
       </oxd-select-option>
       <oxd-select-option
         v-for="(option, i) in computedOptions"
@@ -38,7 +38,7 @@
         @select="onSelect(option)"
       >
         <slot name="option" :data="option"></slot>
-        <span v-if="!$slots['option']">{{ option.label }}</span>
+        <span v-if="!$slots['option']">{{ $vt(option.label) }}</span>
       </oxd-select-option>
     </oxd-select-dropdown>
   </div>
@@ -52,6 +52,7 @@ import {TOP, BOTTOM, Option, Position, DROPDOWN_POSITIONS} from '../types';
 import SelectText from '@orangehrm/oxd/core/components/Input/Select/SelectText.vue';
 import SelectDropdown from '@orangehrm/oxd/core/components/Input/Select/SelectDropdown.vue';
 import SelectOption from '@orangehrm/oxd/core/components/Input/Select/SelectOption.vue';
+import translateMixin from '../../../../mixins/translate';
 
 export default defineComponent({
   name: 'oxd-select-input',
@@ -63,7 +64,7 @@ export default defineComponent({
     'oxd-select-option': SelectOption,
   },
 
-  mixins: [navigationMixin, eventsMixin],
+  mixins: [navigationMixin, eventsMixin, translateMixin],
 
   emits: [
     'update:modelValue',
@@ -143,10 +144,10 @@ export default defineComponent({
     },
     selectedItem(): string {
       return this.modelValue?.label
-        ? this.modelValue.label
+        ? this.$vt(this.modelValue.label)
         : this.hideDropdownDefaultLabel
         ? null
-        : this.placeholder;
+        : this.$vt(this.placeholder);
     },
     inputValue(): string {
       return this.computedOptions[this.pointer]?.label || this.selectedItem;
