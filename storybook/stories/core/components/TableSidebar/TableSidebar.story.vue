4fdf2bedb141176ad48029cd4ed456f51b9ddfcb
Gayan Sandamal <gayansandamal@orangehrmlive.com>
2022-01-20 20:23:19 +0530

    created table list story

--- storybook/stories/core/components/TableSidebar/TableSidebar.story.vue
+++ storybook/stories/core/components/TableSidebar/TableSidebar.story.vue
@@ -1,185 +1,73 @@
 <template>
   <div class="container">
     <oxd-table-sidebar
+      v-if="configurations.table.leftPanel.visible"
       class="oxd-table-left-panel"
-      width="200px"
-      :list="stages"
-      :button="{
-        label: 'Add Candidate',
-        style: {
-          height: '36px'
-        }
-      }"
-      @list:onSelect="selectStage"
+      :class="{'with-filters': configurations.table.topBar.visible}"
+      width="230px"
+      :side-panel-list="dataObj.stages"
+      :header-visible="configurations.table.leftPanel.header.visible"
+      :body-visible="configurations.table.leftPanel.body.visible"
+      :list-visible="configurations.table.leftPanel.list.visible"
+      :bubble-visible="configurations.table.leftPanel.list.bubble.visible"
+      :button="configurations.table.leftPanel.header.button"
+      :selected-list-item-id="1"
+      @sidePanelList:onSelect="sidePanelListOnSelect"
+      @side-panel:onToggle="toggleSidePanel"
+      @sidePanelList:onHeaderBtnClick="sidePanelListOnHeaderBtnClick"
+      :is-side-panel-open="isLeftPanelOpen"
     >
-      <template v-slot:body>
-        <oxd-select-input-btn
-          :button="{
-            label: selectedVacancy && selectedVacancy.label ? selectedVacancy.label : selectedStage && selectedStage.label ? selectedStage.label : '',
-            labelMini: 'Vacancy',
-            iconName: 'eye',
-            displayType: 'label',
-            style: {
-              height: '36px'
-            }
-          }"
-          :options="vacancies"
-          :open-dropdown-initially="true"
-          @update:modelValue="selectVacancy"
-          :modelValue="selectedVacancy"
-        />
+      <template v-slot:sidePanelBody>
+        <slot name="sidePanelBody"></slot>
+      </template>
+      <template v-slot:footer>
+        <slot name="footer"></slot>
       </template>
     </oxd-table-sidebar>
   </div>
 </template>
 
 <script lang="ts">
-import TableSidebar from '@orangehrm/oxd/core/components/TableSidebar/TableSidebar';
+import TableSidebar from '@orangehrm/oxd/core/components/TableSidebar/TableSidebar.vue';
 import SelectInputButton from '@orangehrm/oxd/core/components/Input/Select/SelectInputButton.vue';
+import list from './../List/list-config.json'
+import data from './../List/list-data.json';
 
-export default {
-  data() {
-    return {
-      selectedVacancy: null,
-      vacancies: [
-        {
-          id: 1,
-          label: 'Sales Coordinator'
-        },
-        {
-          id: 2,
-          label: 'Credit Analyst'
-        },
-        {
-          id: 3,
-          label: 'Customer Success Executive'
-        }
-      ],
-      stages: [
-        {
-          id: 1,
-          label: 'Application Received',
-          count: 24,
-          displayType: 'label-info',
-          style: {
-            backgroundColor: '#d1dff6',
-            color: '#1f6ffd'
-          }
-        },
-        {
-          id: 2,
-          label: 'Phone Screening',
-          count: 11,
-          displayType: 'label-warn',
-          style: {
-            backgroundColor: '#ebebf0',
-            color: '#65738f'
-          }
-        },
-        {
-          id: 3,
-          label: '1st In-Person Interview',
-          count: 3,
-          displayType: 'label',
-          style: {
-            backgroundColor: '#ebebf0',
-            color: '#65738f'
-          }
-        },
-        {
-          id: 4,
-          label: 'Shortlisted',
-          count: 0,
-          displayType: 'label-warn',
-          style: {
-            backgroundColor: '#f8ebfd',
-            color: '#c57afd'
-          }
-        },
-        {
-          id: 5,
-          label: 'Panel Interview',
-          count: 9,
-          displayType: 'label-info',
-          style: {
-            backgroundColor: '#ebebf0',
-            color: '#65738f'
-          }
-        },
-        {
-          id: 6,
-          label: 'Reference Check',
-          count: 1,
-          displayType: 'label-warn',
-          style: {
-            backgroundColor: '#ebebf0',
-            color: '#65738f'
-          }
-        },
-        {
-          id: 7,
-          label: '321 Form Onboarding',
-          count: 0,
-          displayType: 'label-warn',
-          style: {
-            backgroundColor: '#ebebf0',
-            color: '#65738f'
-          }
-        },
-        {
-          id: 8,
-          label: 'Job Offer',
-          count: 2,
-          displayType: 'label-success',
-          style: {
-            backgroundColor: '#e1f8e0',
-            color: '#74cb1e'
-          }
-        },
-        {
-          id: 9,
-          label: 'Hired',
-          count: 2,
-          displayType: 'label-warn',
-          style: {
-            backgroundColor: '#fceee3',
-            color: '#ed7d30'
-          }
-        },
-        {
-          id: 10,
-          label: 'Rejected',
-          count: 3,
-          displayType: 'label-danger',
-          style: {
-            backgroundColor: '#f9e3e9',
-            color: '#e74432'
-          }
-        },
-      ]
-    };
-  },
-
+import {defineComponent, ref, computed} from 'vue';
+export default defineComponent({
   components: {
-    'oxd-table-sidebar': TableSidebar,
-    'oxd-select-input-btn': SelectInputButton
+    'oxd-table-sidebar': TableSidebar
   },
+  setup() {
+    const isLeftPanelOpen = ref<boolean>(false)
+    const configurations = computed(() => list)
+    const dataObj = computed(() => {
+      return data
+    })
 
-  computed: {
-    selectedStage() {
-      return this.stages.find(stage => stage.selected)
-    }
-  },
+    const sidePanelListOnSelect = item => {
+      alert(`stage selected ${item}`)
+    };
 
-  methods: {
-    selectVacancy(modelValue) {
-      this.selectedVacancy = modelValue
-    },
-    selectStage(stage) {
-      console.log(stage);
+    const toggleSidePanel = () => {
+      isLeftPanelOpen.value = !isLeftPanelOpen.value
+      alert('toggled')
     }
-  },
-};
+
+    const sidePanelListOnHeaderBtnClick = () => {
+      alert('header button clicked')
+    };
+
+    return {
+      configurations,
+      dataObj,
+      isLeftPanelOpen,
+      sidePanelListOnSelect,
+      toggleSidePanel,
+      sidePanelListOnHeaderBtnClick,
+    }
+  }
+})
 </script>
 
 <style lang="scss" scoped>
