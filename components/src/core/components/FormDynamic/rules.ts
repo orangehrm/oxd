2494b85e7cebf30c26765bf657471cda8d35b6bd
Gayan Sandamal <gayansandamal@orangehrmlive.com>
2022-02-08 03:18:36 +0530

    added char length validations

--- components/src/core/components/FormDynamic/rules.ts
+++ components/src/core/components/FormDynamic/rules.ts
@@ -6,6 +6,12 @@ export const rulesList = [
     "name": (v: any) => (v && v.length <= 30) || 'Should be less than 30 characters',
   },
   {
+    "url": (v: any) => (v && v.length <= 100) || 'Should be less than 100 characters',
+  },
+  {
+    "keywords": (v: any) => (v && v.length <= 254) || 'Should be less than 254 characters',
+  },
+  {
     "email": (v: any) => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid',
   },
   {
