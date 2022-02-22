029327160410fc2794942ed8db1bb79fe50d9646
Gayan Sandamal <gayansandamal@orangehrmlive.com>
2022-02-22 17:17:11 +0530

    BUG FIX - RMP-358

--- components/src/core/components/TableSidebar/TableSidebar.vue
+++ components/src/core/components/TableSidebar/TableSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="oxd-table-left-panel" :style="customStyles">
-    <div v-if="headerVisible" class="header">
+    <div v-if="headerVisible" class="oxd-table-left-panel--header">
       <slot name="header">
         <oxd-button
           class="table-header-action-btn"
@@ -20,10 +20,10 @@
         </oxd-button>
       </slot>
     </div>
-    <div v-if="bodyVisible" class="body">
+    <div v-if="bodyVisible" class="oxd-table-left-panel--body">
       <slot name="sidePanelBody"></slot>
     </div>
-    <div v-if="listVisible" class="footer list">
+    <div v-if="listVisible" class="oxd-table-left-panel--footer list">
       <slot name="footer">
         <ul>
           <li
@@ -54,7 +54,7 @@
     </div>
     <oxd-icon-button
       :name="isLeftPanelOpen ? 'chevron-left' : 'chevron-right'"
-      class="oxd-table-left-panel-toggle-btn"
+      class="oxd-table-left-panel--toggle-btn"
       @click="toggleLeftPanel"
     />
   </div>
