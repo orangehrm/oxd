0faceea1604f40448e139f3aa191136664af9661
imesh-orangehrm <100343455+imesh-orangehrm@users.noreply.github.com>
2022-02-27 23:10:58 +0530

    Add progress bar component

---
+++ storybook/stories/core/components/ProgressBar/ProgressBar.stories.js
@@ -0,0 +1,21 @@
+import ProgressBar from '@orangehrm/oxd/core/components/ProgressBar/ProgressBar';
+
+export default {
+  title: 'Example/ProgressBar/Linear',
+  component: ProgressBar,
+};
+
+const Template = args => ({
+  setup() {
+    return {args};
+  },
+  components: {'oxd-progress-bar': ProgressBar},
+  template: '<oxd-progress-bar v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Exporting Candidate List to CSV',
+  show: true,
+};
+
