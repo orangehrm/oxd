499003f14f6ea6a5b38d1c5d80030887bbfc5f48
Ruchira Amarasinghe <rtamarasinghe@gmail.com>
2022-03-01 22:55:55 +0530

    add test to verify sort order changes in List component

--- components/src/core/components/List/__tests__/list.spec.ts
+++ components/src/core/components/List/__tests__/list.spec.ts
@@ -1,14 +1,33 @@
-import {mount} from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
 import List from "@orangehrm/oxd/core/components/List/List.vue";
 import minimalListConfig from './list-minimal.json'
 
 describe('List > List.vue', () => {
-  it('renders OXD List > List', () => {
-    const wrapper = mount(List, {
+  it('correctly computes initial sort order', () => {
+    const wrapper = shallowMount(List, {
       props: {
         configurations: minimalListConfig,
       },
     });
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.vm.order).toStrictEqual({
+      'candidate': 'DEFAULT', 'email': 'DEFAULT', 'contactNumber': 'DEFAULT', 'dateOfApplication': 'DESC'});
+  });
+
+  it('changes sort order when sort changes', () => {
+    const wrapper = shallowMount(List, {
+      props: {
+        configurations: minimalListConfig,
+      },
+    });
+    wrapper.vm.tableSort({'candidate': 'ASC', 'email': 'DEFAULT', 'contactNumber': 'DEFAULT', 'dateOfApplication': 'DEFAULT'});
+
+    // assert event has been emitted
+    expect(wrapper.emitted()['update:order']).toBeTruthy()
+    expect(wrapper.emitted()['update:order'].length).toBe(1)
+    expect(wrapper.emitted()['update:order'][0]).toStrictEqual([{'candidate': 'ASC', 'email': 'DEFAULT', 'contactNumber': 'DEFAULT', 'dateOfApplication': 'DEFAULT'}])
+
+    // assert sort order has been updated
+    expect(wrapper.vm.order).toStrictEqual({
+      'candidate': 'ASC', 'email': 'DEFAULT', 'contactNumber': 'DEFAULT', 'dateOfApplication': 'DEFAULT'});
   });
 });
