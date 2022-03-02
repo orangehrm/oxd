b2fc5f3de07f5fe6a8172cc4662aa72b4253481f
Gayan Sandamal <gayansandamal@orangehrmlive.com>
2022-03-02 17:56:43 +0530

    Bug fix - RMP-334 The candidate's Profile image doesn't have a column header in the candidate list

--- storybook/stories/core/components/CardTable/Table.stories.js
+++ storybook/stories/core/components/CardTable/Table.stories.js
@@ -5,6 +5,9 @@ import SortableCardTable from './SortableCardTable.story.vue';
 import CardTableJobs from './CardTableJobs.story.vue';
 import CardTableRowOptions from './CardTableRowOptions.story.vue';
 import CardTableHighlight from './CardTableHighlight.story.vue';
+import CardTableIconHeaders from './CardTableIconHeaders.story.vue';
+
+import candidateAvatars from './../List/candidate-avatars.json'
 
 export default {
   title: 'Example/CardTable',
@@ -45,9 +48,19 @@ const HighlightTemplate = args => ({
   template: '<CardTableHighlight v-bind="args" />',
 });
 
+const IconHeadersTemplate = args => ({
+  setup() {
+    return {args};
+  },
+  components: {CardTableIconHeaders},
+  template: '<CardTableIconHeaders v-bind="args" />',
+});
+
 export const RowOptions = RowOptionsTemplate.bind({});
 
 export const HighlightExample = HighlightTemplate.bind({});
+
+export const IconHeadersExample = IconHeadersTemplate.bind({});
 
 RowOptions.args = {
   selectable: true,
@@ -65,4 +78,57 @@ HighlightExample.args = {
     rowIndexes: [0,2],
     type: 'success',
   }
+};
+
+
+IconHeadersExample.args = {
+  headers: [
+    {
+      name: 'profilePic',
+      title: 'Column 1',
+      iconName: 'oxd-profile-pic',
+      iconStyle: 'color: #929baa;',
+      sortable: true,
+      style: {
+        width: '45px',
+        'justify-content': 'center'
+      },
+      cellType: 'oxd-table-cell-profile-pic',
+    },
+    {
+      name: 'col1',
+      title: 'Column 1',
+      sortable: true,
+      style: {
+        flex: 1,
+        color: '#929baa',
+      },
+    },
+    {
+      name: 'col2',
+      title: 'Column 2',
+      style: {
+        flex: 5,
+        color: '#929baa',
+      },
+    },
+  ],
+  items: [
+    {
+      profilePic: candidateAvatars.avatars[0],
+      col1: 'Data 1',
+      col2: 'Data 2',
+    },
+    {
+      profilePic: candidateAvatars.avatars[1],
+      col1: 'Data 2',
+      col2: 'Data 2',
+    },
+    {
+      profilePic: candidateAvatars.avatars[2],
+      col1: 'Data 3',
+      col2:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+  ],
 };
