f98aaa6ac03942576e2b8d1ca1ff07250eaac986
Ruchira Amarasinghe <rtamarasinghe@gmail.com>
2022-03-03 06:55:31 +0530

    * Add padding to top/bottom of dropdown scrollbar * Show 5 items in dropdown * If empty option --select-- is there, show 6 items

--- components/src/core/components/Input/Select/SelectInput.vue
+++ components/src/core/components/Input/Select/SelectInput.vue
@@ -132,6 +132,7 @@ export default defineComponent({
       return {
         '--positon-bottom': this.dropdownPosition === BOTTOM,
         '--positon-top': this.dropdownPosition === TOP,
+        '--with-empty-selector': this.showEmptySelector && !this.hideDropdownDefaultLabel
       };
     },
     optionClasses(): object[] {
