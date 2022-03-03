6ec9900c20a831e9cc5d690dc07bd6f6e5251a04
Gayan Sandamal OrangeHRM <93513174+gayan-orangehrm@users.noreply.github.com>
2022-03-03 15:16:40 +0530

    Add icon component to cell header
    * Bug fix - RMP-334 The candidate's Profile image doesn't have a column header in the candidate list

---
+++ storybook/stories/core/components/CardTable/CardTableIconHeaders.story.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="orangehrm-container">
+    <oxd-card-table
+      :headers="headers"
+      :items="items"
+      rowDecorator="oxd-table-decorator-card"
+    />
+  </div>
+</template>
+
+<script>
+import CardTable from '@orangehrm/oxd/core/components/CardTable/CardTable';
+
+export default {
+  props: {
+    headers: {
+      type: Array,
+    },
+    items: {
+      type: Array,
+    },
+  },
+
+  components: {
+    'oxd-card-table': CardTable,
+  },
+
+  methods: {
+    onClick(e) {
+      console.log(e);
+    },
+  },
+};
+</script>
+
+<style>
+.orangehrm-container {
+  background-color: #f6f5fb;
+  border-radius: 1.2rem;
+  padding: 0.5rem;
+}
+</style>
