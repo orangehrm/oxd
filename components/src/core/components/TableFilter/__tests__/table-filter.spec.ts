b66870a39e19f29c1d192802a424ec67acb10c2b
amilaw <sandaruwan@orangehrm.us.com>
2022-03-03 18:28:09 +0530

    adding tests to table-filter

--- components/src/core/components/TableFilter/__tests__/table-filter.spec.ts
+++ components/src/core/components/TableFilter/__tests__/table-filter.spec.ts
@@ -2,12 +2,26 @@ import {mount, shallowMount} from '@vue/test-utils';
 import TableFilter from '@orangehrm/oxd/core/components/TableFilter/TableFilter.vue';
 
 describe('TableFilter', () => {
-  it('renders', () => {
+  it('Renders', () => {
     const wrapper = shallowMount(TableFilter, {
       props: {
         filterTitle: '1 item slected',
       },
     });
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('Checked action buttons', () => {
+    const wrapper = shallowMount(TableFilter, {
+      props: {
+        filterTitle: '1 item slected',
+      },
+      slots: {
+        actionOptions: '<div id="actionOptions">Action Options</div>',
+      },
+    });
+    expect(wrapper.find('.--actions').html()).toContain(
+      '<div id="actionOptions">Action Options</div>',
+    );
   });
 });
