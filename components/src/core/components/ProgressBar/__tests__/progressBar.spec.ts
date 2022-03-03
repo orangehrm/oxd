776ab32861d2cb0b6ed49fc5d636ad8f1448417d
imesh-orangehrm <100343455+imesh-orangehrm@users.noreply.github.com>
2022-03-03 06:26:40 +0530

    Update progress indicator classes and styles
    * modal display block
    * Modify overriding classes and adding overlay

--- components/src/core/components/ProgressBar/__tests__/progressBar.spec.ts
+++ components/src/core/components/ProgressBar/__tests__/progressBar.spec.ts
@@ -19,8 +19,6 @@ describe('ProgressBar > ProgressBar.vue', () => {
         show: true,
       },
     });
-    expect(wrapper.html()).toContain(
-      '<h5 class="modal-title">List to CSV</h5>',
-    );
+    expect(wrapper.html()).toContain("<h5 class=\"progress-modal-title\">List to CSV</h5>");
   });
 });
