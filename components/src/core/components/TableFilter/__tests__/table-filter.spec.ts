5c56c56269e1c07ced6d63e500775fa21a3e14e4
Gayan Sandamal <gayansandamal@orangehrmlive.com>
2022-03-09 15:08:47 +0530

    resolved lint issues

--- components/src/core/components/TableFilter/__tests__/table-filter.spec.ts
+++ components/src/core/components/TableFilter/__tests__/table-filter.spec.ts
@@ -1,4 +1,4 @@
-import {mount, shallowMount} from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
 import TableFilter from '@orangehrm/oxd/core/components/TableFilter/TableFilter.vue';
 
 describe('TableFilter', () => {
