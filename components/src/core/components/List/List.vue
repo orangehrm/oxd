00d0a93a39b3cfffe7ec6dff025d5e2462a45872
Ruchira <ruchira@orangehrm.com>
2022-03-09 20:05:28 +0530

    Add list component UI changes to increase space available to table
    Reduce the width of the side panel to 200px
    Settable to flex-grow
    Prevent sidebar shrinking.
    Remove extra padding on the table right

--- components/src/core/components/List/List.vue
+++ components/src/core/components/List/List.vue
@@ -10,7 +10,7 @@
       v-if="config.table.leftPanel.visible"
       class="oxd-table-left-panel"
       :class="{'with-filters': config.table.topBar.visible}"
-      width="250px"
+      width="200px"
       :side-panel-list="sidePanelList"
       :header-visible="config.table.leftPanel.header.visible"
       :body-visible="config.table.leftPanel.body.visible"
