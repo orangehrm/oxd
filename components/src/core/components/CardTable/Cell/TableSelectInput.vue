2fd7e1a78339974a76cc6038a3de6d15ff80210d
Ruchira <ruchira@orangehrm.com>
2022-03-10 10:56:55 +0530

    Add wrapper for select input for use in list/table
    * wrapper for select input for use in list/table
    * keep select wrapper width at 100%

---
+++ components/src/core/components/CardTable/Cell/TableSelectInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <oxd-select-input
+    up-icon="chevron-up"
+    down-icon="chevron-down"
+  ></oxd-select-input>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import SelectInput from '@orangehrm/oxd/core/components/Input/Select/SelectInput.vue';
+
+export default defineComponent({
+  name: 'oxd-table-select-input',
+  components: {
+    'oxd-select-input': SelectInput,
+  },
+});
+</script>
+
+<style src="./table-select-input.scss" lang="scss" scoped></style>
