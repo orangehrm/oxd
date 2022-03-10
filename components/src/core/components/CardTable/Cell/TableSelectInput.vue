29766e7bf95f18b2a67ba26e9918d018f9f91669
Ruchira Amarasinghe <rtamarasinghe@gmail.com>
2022-03-10 08:58:08 +0530

    wrapper for select input for use in list/table

---
+++ components/src/core/components/CardTable/Cell/TableSelectInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <oxd-select-input
+    v-bind="$attrs"
+    :options="options"
+    up-icon="chevron-up"
+    down-icon="chevron-down"
+  ></oxd-select-input>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import SelectInput from '@orangehrm/oxd/core/components/Input/Select/SelectInput.vue';
+
+export default defineComponent({
+  name: 'oxd-table-select-input',
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    'oxd-select-input': SelectInput,
+  },
+});
+</script>
+
+<style src="./table-select-input.scss" lang="scss" scoped></style>
