73f42b44601493999af0eb7760aa1154c7b8111a
Chamara Abesinghe <chamara@orangehrmlive.com>
2022-03-16 14:53:18 +0530

    RMP-438: OXD-schema-form model prop added, useSchemaForm composable added, story updated

--- components/src/core/components/SchemaForm/types.ts
+++ components/src/core/components/SchemaForm/types.ts
@@ -35,6 +35,9 @@ const LAYOUT_TYPE_MAP: TypeMap<LayoutComponent> = {
 type Props = Record<string, any>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Model = {[key: string]: any};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Validator = (value: any) => boolean | string;
 
 type CommonSchemaProperties = {
@@ -63,7 +66,6 @@ type FieldSchema = CommonSchemaProperties &
     value?: string | number | object | unknown;
     placeholder?: string;
     visible?: boolean;
-    required?: boolean | Validator;
     hook?: (schema: FieldSchema, model: object) => FieldSchema;
     validators?: Array<Validator>;
     listeners?: EventListeners;
@@ -84,6 +86,7 @@ type FormSchema = CommonSchemaProperties & {
 
 export {
   Props,
+  Model,
   FieldType,
   LayoutType,
   LayoutChild,
