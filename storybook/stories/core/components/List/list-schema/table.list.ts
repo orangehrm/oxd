8c7b1add5ac63aad476c15c092d41df5d2453011
amilaw <sandaruwan@orangehrm.us.com>
2022-03-31 08:08:54 +0530

    removeing unwanted line

--- storybook/stories/core/components/List/list-schema/table.list.ts
+++ storybook/stories/core/components/List/list-schema/table.list.ts
@@ -2,8 +2,6 @@ import leftPanel from './left-panel.table.list';
 import topBar from './top-bar.table.list';
 import headers from './headers.table.list';
 
-import {ListTable} from '../../../../../../components/src/core/components/List/types';
-
 const table = {
   leftPanel: leftPanel,
   topBar: topBar,
