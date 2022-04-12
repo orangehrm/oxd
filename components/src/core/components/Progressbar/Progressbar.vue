ce5fe1791c6804e7684fede2a3f1ccc48b1e57be
Chamara Abesinghe <chamara@orangehrmlive.com>
2022-04-12 13:16:39 +0530

    OHRM5X-819: OXD-Progressbar component added

---
+++ components/src/core/components/Progressbar/Progressbar.vue
@@ -0,0 +1,90 @@
+<!--
+/*
+ * This file is part of OrangeHRM Inc
+ *
+ * Copyright (C) 2020 onwards OrangeHRM Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see  http://www.gnu.org/licenses
+ */
+-->
+
+<template>
+  <div class="oxd-progressbar">
+    <div class="oxd-progressbar-wrapper">
+      <div :style="styles" :class="classes"></div>
+    </div>
+    <oxd-text v-if="showLabel" class="oxd-progressbar-text" tag="p">
+      {{ progressText }}
+    </oxd-text>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {ProgressbarType, TYPE_DEFAULT, TYPES} from './types';
+import Text from '@ohrm/oxd/core/components/Text/Text.vue';
+
+export default defineComponent({
+  name: 'oxd-progressbar',
+  props: {
+    showLabel: {
+      type: Boolean,
+      default: true,
+    },
+    stripe: {
+      type: Boolean,
+      default: false,
+    },
+    animation: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: TYPE_DEFAULT,
+      validator: function(value: ProgressbarType) {
+        return TYPES.indexOf(value) !== -1;
+      },
+    },
+  },
+  components: {
+    'oxd-text': Text,
+  },
+  data() {
+    return {
+      progress: 50,
+    };
+  },
+  computed: {
+    styles(): object {
+      return {
+        width: `${this.progress}%`,
+      };
+    },
+    classes(): object {
+      return {
+        'oxd-progressbar-ticker': true,
+        [`oxd-progressbar-ticker--${this.type}`]: true,
+        '--striped': this.stripe,
+        '--animated': this.animation,
+      };
+    },
+    progressText(): string {
+      return `${Math.round(this.progress)}%`;
+    },
+  },
+});
+</script>
+
+<style src="./progressbar.scss" lang="scss" scoped></style>
---
+++ storybook/stories/core/components/Progressbar/Progressbar.stories.js
@@ -0,0 +1,65 @@
+/*
+ * This file is part of OrangeHRM Inc
+ *
+ * Copyright (C) 2020 onwards OrangeHRM Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see  http://www.gnu.org/licenses
+ */
+
+import Progressbar from '@ohrm/oxd/core/components/Progressbar/Progressbar.vue';
+import {TYPES} from '@ohrm/oxd/core/components/Progressbar/types';
+
+export default {
+  title: 'Example/Progressbar',
+  component: Progressbar,
+  argTypes: {
+    type: {
+      control: {type: 'select', options: TYPES},
+    },
+  },
+};
+
+const Template = args => ({
+  setup() {
+    return {args};
+  },
+  components: {'oxd-progressbar': Progressbar},
+  template: '<oxd-progressbar v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  type: 'default',
+};
+
+export const WithoutLabel = Template.bind({});
+WithoutLabel.args = {
+  showLabel: false,
+  type: 'secondary',
+};
+
+export const WithStripes = Template.bind({});
+WithStripes.args = {
+  stripe: true,
+  showLabel: false,
+  type: 'main',
+};
+
+export const WithAnimation = Template.bind({});
+WithAnimation.args = {
+  stripe: true,
+  animation: true,
+  showLabel: false,
+  type: 'warn',
+};
