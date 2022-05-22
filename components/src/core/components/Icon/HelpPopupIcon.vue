350d885ee9bff984f96f42a4958e2d828fa09b3f
Ruchira <ruchira@orangehrm.com>
2022-05-22 15:45:24 +0530

    Help popup icon
    * Help popup for switch input
    * reduced icon size to small, removed unused constant
    * support for an optional help icon for switch input
    * add useFullWidth option to switchInput
    * support for positioning help popup inside switch input.
    * prevent switch getting squashed by settign a min-width
    * setting help-popup-icon currentColor
    * make help popup non-transparent (opacity to 1)

---
+++ components/src/core/components/Icon/HelpPopupIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <oxd-icon
+    :size="ICON_SIZE_SMALL"
+    name="oxd-help-popup"
+    :help-tooltip="$vt(helpText)"
+    :position="helpPosition"
+  />
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import Icon from '@orangehrm/oxd/core/components/Icon/Icon.vue';
+import {HelpPosition, BOTTOM_RIGHT, HELP_POSITIONS} from './types';
+import translateMixin from '@orangehrm/oxd/mixins/translate';
+import {ICON_SIZE_SMALL} from '@orangehrm/oxd/core/components/Icon/types';
+
+export default defineComponent({
+  name: 'oxd-help-popup-icon',
+
+  components: {
+    'oxd-icon': Icon,
+  },
+
+  mixins: [translateMixin],
+
+  data() {
+    return {
+      ICON_SIZE_SMALL,
+    };
+  },
+
+  props: {
+    helpText: {
+      type: String,
+      default: null,
+    },
+    helpPosition: {
+      type: String,
+      default: BOTTOM_RIGHT,
+      validator: (value: HelpPosition) => {
+        return HELP_POSITIONS.indexOf(value) !== -1;
+      },
+    },
+  },
+});
+</script>
+<style src="./help-popup-icon.scss" lang="scss" scoped></style>
