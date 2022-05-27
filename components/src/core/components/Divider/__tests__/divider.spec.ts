f31fdb0feb09dc55b197756a4b7a8a39476c3feb
Shachini Mekala <105478406+shachiniM@users.noreply.github.com>
2022-05-27 14:41:10 +0530

    Add vertical divider support for the existing divider component

--- components/src/core/components/Divider/__tests__/divider.spec.ts
+++ components/src/core/components/Divider/__tests__/divider.spec.ts
@@ -15,4 +15,28 @@ describe('Divider.vue', () => {
     });
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  it('render horizontal OXD Divider with default orientation', () => {
+    const wrapper = mount(Divider, {
+      props: {},
+    });
+    expect(wrapper.findAll(".oxd-divider-horizontal").length).toBe(1);
+    expect(wrapper.findAll(".oxd-divider-vertical").length).toBe(0);
+  });
+
+  it('render vertical OXD Divider when pass through props', () => {
+    const wrapper = mount(Divider, {
+      props: { orientation : 'vertical'},
+    });
+    expect(wrapper.findAll(".oxd-divider-vertical").length).toBe(1);
+    expect(wrapper.findAll(".oxd-divider-horizontal").length).toBe(0);
+  });
+
+  it('render horizontal OXD Divider when invalid prop value for orientation is passed', () => {
+    const wrapper = mount(Divider, {
+      props: { orientation : 'this is invalid'},
+    });
+    expect(wrapper.findAll(".oxd-divider-horizontal").length).toBe(1);
+    expect(wrapper.findAll(".oxd-divider-vertical").length).toBe(0);
+  });
 });
