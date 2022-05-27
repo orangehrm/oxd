c722559a9a5dc48e0247a08afc6ebbab8a41f64e
Thilina Sumithrarachchi <thilinasan166@gmail.com>
2022-05-27 11:28:34 +0530

    oxd-select updates

--- components/src/core/components/Input/Select/SelectText.vue
+++ components/src/core/components/Input/Select/SelectText.vue
@@ -1,17 +1,26 @@
 <template>
   <div
-    v-bind="$attrs"
     :class="classes"
     :style="style"
     :tabindex="tabIndex"
+    v-bind="defaultAttrs"
     @focus="onFocus"
     @blur="onBlur"
   >
     <div class="oxd-select-text-input">
-      {{ value }}
+      <div class="selected-content">{{ value }}</div>
+      <input
+        type="text"
+        readonly="readonly"
+        v-bind="inputAttrs"
+        @focus="onFocus"
+        @blur="$attrs.onBlur"
+      />
+    </div>
+    <div class="oxd-select-text--after-slot-option">
+      <slot name="afterInput"></slot>-
     </div>
     <div class="oxd-select-text--after">
-      <slot name="afterInput"></slot>
       <div :class="dropdownIconClasses">
         <oxd-icon
           v-if="!disabled"
@@ -107,6 +116,24 @@ export default defineComponent({
     },
     tabIndex(): number {
       return this.disabled || this.readonly ? -1 : 0;
+    },
+    inputAttrs() {
+      const allowed = ['id'];
+      return Object.keys(this.$attrs)
+        .filter(key => allowed.includes(key))
+        .reduce((obj, key) => {
+          obj[key] = this.$attrs[key];
+          return obj;
+        }, {});
+    },
+    defaultAttrs() {
+      const notAllowed = ['id'];
+      return Object.keys(this.$attrs)
+        .filter(key => !notAllowed.includes(key))
+        .reduce((obj, key) => {
+          obj[key] = this.$attrs[key];
+          return obj;
+        }, {});
     },
   },
 
