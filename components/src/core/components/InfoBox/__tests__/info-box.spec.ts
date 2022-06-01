1c751625d9f3729971bf8c8b58d8a9e635c046dc
gayanSandamal <gayansandamal94@gmail.com>
2022-06-02 03:00:49 +0530

    RMP-685 - added more unit tests including color converter method

--- components/src/core/components/InfoBox/__tests__/info-box.spec.ts
+++ components/src/core/components/InfoBox/__tests__/info-box.spec.ts
@@ -2,6 +2,7 @@ import {mount} from '@vue/test-utils';
 import InfoBox from '@orangehrm/oxd/core/components/InfoBox/InfoBox.vue';
 import ButtonIcon from '@orangehrm/oxd/core/components/Button/Icon.vue';
 import SelectOption from '@orangehrm/oxd/core/components/Input/Select/SelectOption.vue';
+import { BOTTOM } from '../../Input/types';
 
 const options = [
   {
@@ -35,26 +36,32 @@ const options = [
     label: 'orem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.'
   }
 ];
+const label = 'Current Stage of Recruitment';
+const rows = 3;
+const dropdownPosition = BOTTOM;
 
 describe('InfoBox.vue', () => {
+
   it('renders OXD Select Input', () => {
     const wrapper = mount(InfoBox, {
-      props: {options},
+      props: {label, rows, options, dropdownPosition},
     });
     expect(wrapper.html()).toMatchSnapshot();
   });
+
   it('should load options to Select', async () => {
     const wrapper = mount(InfoBox, {
-      props: {options},
+      props: {label, rows, options, dropdownPosition},
     });
     wrapper.findComponent(ButtonIcon).trigger('click');
     await wrapper.vm.$nextTick();
     const nodes = wrapper.findAllComponents(SelectOption);
     expect(nodes.length).toBe(7);
   });
+
   it('should select one option', async () => {
     const wrapper = mount(InfoBox, {
-      props: {options},
+      props: {label, rows, options, dropdownPosition},
     });
     wrapper.findComponent(ButtonIcon).trigger('click');
     await wrapper.vm.$nextTick();
@@ -70,4 +77,38 @@ describe('InfoBox.vue', () => {
       ],
     ]);
   });
+
+  it('should not open the dropdown when the button icon is clicked but the component is disabled', async () => {
+    const wrapper = mount(InfoBox, {
+      props: {
+        label,
+        rows,
+        options,
+        dropdownPosition,
+        disabled: true,
+      },
+    });
+    wrapper.findComponent(ButtonIcon).trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted('dropdown:opened')).toBeFalsy();
+    expect(wrapper.vm.dropdownOpen).toEqual(false);
+    expect(wrapper.find('.oxd-select-dropdown').exists()).toBeFalsy();
+  });
+
+  it('should close the dropdown when clicked outside', async () => {
+    const wrapper = mount(InfoBox, {
+      props: {
+        label,
+        rows,
+        options,
+        dropdownPosition,
+      },
+    });
+    wrapper.trigger('blur');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted('dropdown:opened')).toBeFalsy();
+    expect(wrapper.vm.dropdownOpen).toEqual(false);
+    expect(wrapper.find('.oxd-select-dropdown').exists()).toBeFalsy();
+  });
+
 });
