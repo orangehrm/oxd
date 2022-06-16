e12f52662d49217692159c1b48c758d32013894c
gayanSandamal <gayansandamal94@gmail.com>
2022-06-16 12:43:17 +0530

    RMP-709 - Updated unit test as per the PR review meeting

--- components/src/core/components/Pagination/__tests__/pagination.spec.ts
+++ components/src/core/components/Pagination/__tests__/pagination.spec.ts
@@ -1,5 +1,14 @@
-import {shallowMount} from '@vue/test-utils';
+import {mount, shallowMount} from '@vue/test-utils';
 import Pagination from '@orangehrm/oxd/core/components/Pagination/Pagination.vue';
+// import SelectInput from '@orangehrm/oxd/core/components/Input/Select/SelectInput.vue';
+
+const delayFunction = (time: number) => {
+  return new Promise(reslove =>
+    setTimeout(() => {
+      reslove(true);
+    }, time),
+  );
+};
 
 describe('Pageination.vue', () => {
   it('renders OXD Pagination', () => {
@@ -10,15 +19,20 @@ describe('Pageination.vue', () => {
     });
     expect(wrapper.html()).toMatchSnapshot();
   });
-  it('customize pageList and changed the initial selected page', () => {
-    const wrapper = shallowMount(Pagination, {
+  it('customize pageList and changed the initial selected page', async () => {
+    const wrapper = mount(Pagination, {
       props: {
         length: 20,
         pagesList: [5, 10, 20, 50, 100, 200],
         perPage: 50,
       },
     });
-    expect(wrapper.props().pagesList).toMatchObject([5, 10, 20, 50, 100, 200]);
-    expect(wrapper.props().perPage).toBe(50);
+    const selectInputWrapper = wrapper.find('.pagination-dropdown');
+    await selectInputWrapper.trigger('click');
+    expect(
+      wrapper.find('.oxd-select-wrapper > .oxd-select-dropdown > .oxd-select-dropdown-inner > .oxd-select-option:nth-child(4)').html(),
+    ).toEqual(
+      '<div role="option" class="oxd-select-option --selected"><span>50</span></div>',
+    );
   });
 });
