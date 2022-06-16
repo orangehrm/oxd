31aa8a2695cbed3394f482c835a954537f8a66e6
Gayan Sandamal OrangeHRM <93513174+gayan-orangehrm@users.noreply.github.com>
2022-06-16 09:37:05 +0530

    Removed commented code blocks

--- components/src/core/components/Pagination/__tests__/pagination.spec.ts
+++ components/src/core/components/Pagination/__tests__/pagination.spec.ts
@@ -20,7 +20,5 @@ describe('Pageination.vue', () => {
     });
     expect(wrapper.props().pagesList).toMatchObject([5, 10, 20, 50, 100, 200]);
     expect(wrapper.props().perPage).toBe(50);
-    // console.log(wrapper.find('.selected-content').html());
-    // expect(wrapper.find('.selected-content'));
   });
 });
