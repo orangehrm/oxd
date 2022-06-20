7e2a23522fb1f9e121d6dfa31ac9e90ac4d0e4f4
Ruchira Amarasinghe <rtamarasinghe@gmail.com>
2022-06-20 12:30:10 +0530

    add tests for Alert.vue compact property

--- components/src/core/components/Alert/__tests__/alert.spec.ts
+++ components/src/core/components/Alert/__tests__/alert.spec.ts
@@ -12,4 +12,32 @@ describe('Alert.vue', () => {
     });
     expect(wrapper.html()).toMatchSnapshot();
   });
+  it('Compact Alert', () => {
+    const wrapper = shallowMount(Alert, {
+      props: {
+        show: true,
+        type: 'success',
+        message: 'This is an alert',
+        compact: true,
+      },
+    });
+    expect(wrapper.findAll('.oxd-alert.oxd-alert--compact').length).toEqual(1);
+    expect(
+      wrapper.findAll('.oxd-alert-content.oxd-alert-content--compact').length,
+    ).toEqual(1);
+  });
+  it('Non Compact Alert', () => {
+    const wrapper = shallowMount(Alert, {
+      props: {
+        show: true,
+        type: 'success',
+        message: 'This is an alert',
+        compact: false,
+      },
+    });
+    expect(wrapper.findAll('.oxd-alert.oxd-alert--compact').length).toEqual(0);
+    expect(
+      wrapper.findAll('.oxd-alert-content.oxd-alert-content--compact').length,
+    ).toEqual(0);
+  });
 });
