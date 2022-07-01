b158a6911e6a5c6f61b5624b301391b781b0c9ac
yasas4d <yasassandeepa007@gmail.com>
2022-07-01 18:17:57 +0530

    Add test case file

--- components/src/core/components/CardTable/Header/__tests__/default-card-header.spec.ts
+++ components/src/core/components/CardTable/Header/__tests__/default-card-header.spec.ts
@@ -1,79 +1,10 @@
 import {shallowMount} from '@vue/test-utils';
 import DefaultCardHeader from '@orangehrm/oxd/core/components/CardTable/Header/DefaultCardHeader.vue';
 import emitter from '@orangehrm/oxd/utils/emitter';
+import {getCheckIconTestCases} from './default-card-header-test-cases';
 
 describe('DefaultCardHeader > DefaultCardHeader.vue', () => {
-  const dataSet = [
-    {
-      message: 'renders OXD CardTable > TableBody with selected one',
-      allItems: [
-        {id: 1, isSelectable: true, isDisabled: false},
-        {id: 2},
-        {id: 3},
-      ],
-      selected: [1],
-      rowToSelect: 2,
-      icon: 'dash',
-    },
-    {
-      message: 'renders OXD CardTable > TableBody with selecting all',
-      allItems: [
-        {id: 1, isSelectable: true, isDisabled: false},
-        {id: 2},
-        {id: 3},
-      ],
-      rowToSelect: 3,
-      selected: [1, 2],
-      icon: 'check',
-    },
-    {
-      message: 'renders OXD CardTable > TableBody with unselecting one',
-      allItems: [
-        {id: 1, isSelectable: true, isDisabled: false},
-        {id: 2},
-        {id: 3},
-      ],
-      rowToUnSelect: 3,
-      selected: [1, 2, 3],
-      icon: 'dash',
-    },
-    {
-      message: 'renders OXD CardTable > TableBody with selectable false',
-      allItems: [
-        {id: 1, isSelectable: false, isDisabled: false},
-        {id: 2},
-        {id: 3},
-      ],
-      rowToSelect: 3,
-      selected: [2],
-      icon: 'check',
-    },
-    {
-      message:
-        'renders OXD CardTable > TableBody with selectable false and unchecked',
-      allItems: [
-        {id: 1, isSelectable: false, isDisabled: false},
-        {id: 2},
-        {id: 3},
-      ],
-      rowToUnSelect: 3,
-      selected: [2, 3],
-      icon: 'dash',
-    },
-    {
-      message: 'renders OXD CardTable > TableBody with disabled',
-      allItems: [
-        {id: 1, isSelectable: false, isDisabled: true},
-        {id: 2},
-        {id: 3},
-      ],
-      rowToSelect: 3,
-      selected: [2],
-      icon: 'check',
-    },
-  ];
-
-  dataSet.forEach(testCase => {
+  getCheckIconTestCases.forEach(testCase => {
     it(`${testCase.message}`, async () => {
       const {allItems, selected, icon, rowToSelect, rowToUnSelect} = testCase;
       const wrapper = shallowMount(DefaultCardHeader, {
