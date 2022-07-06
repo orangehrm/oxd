465ed0e0de13fac9b198152347847efeebf284c1
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-07-06 14:40:55 +0530

    OHRM5X-1507: Append version to title

--- storybook/.storybook/manager.js
+++ storybook/.storybook/manager.js
@@ -1,9 +1,10 @@
 import {addons} from '@storybook/addons';
 import {create} from '@storybook/theming';
+import {version} from '../../package.json';
 
 const orangehrmTheme = create({
   base: 'light',
-  brandTitle: 'OXD Storybook',
+  brandTitle: `OXD Storybook - ${version}`,
   brandUrl: 'https://www.orangehrm.com',
   brandImage:
     'https://www.orangehrm.com/themes/orangehrm-modern/static/images/logo.png',
