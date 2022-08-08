77d36659e9a0d083d5cd1d023164e4e02c5c337a
nimnakapre <69051471+nimnakapre@users.noreply.github.com>
2022-08-08 13:56:59 +0530

    Implement PopOver component

---
+++ components/src/core/components/PopOver/PopOver.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="oxd-pop-over">
+    <div
+        @click="openPopOver"
+        class="oxd-pop-over-button"
+    >
+      <slot name="button"></slot>
+    </div>
+    <transition name="transition-fade-down">
+      <div
+          class="oxd-pop-over-content"
+          v-if="isActive"
+          v-click-outside="closePopOver"
+          role="dialog"
+          v-dropdown-direction
+          v-horizontal-direction
+
+      >
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, watch} from 'vue';
+import clickOutsideDirective from '../../../directives/click-outside';
+import dropdownDirectionDirective from '../../../directives/dropdown-direction';
+import popoverHorizontalDirectionDirectiveDirectionDirective from '../../../directives/popover-direction';
+
+
+export default defineComponent({
+  name: 'oxd-pop-over',
+
+  directives: {
+    'click-outside': clickOutsideDirective,
+    'dropdown-direction': dropdownDirectionDirective,
+    'horizontal-direction': popoverHorizontalDirectionDirectiveDirectionDirective,
+  },
+
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  emits: ['update:show'],
+
+  setup: function (props: any) {
+    const isActive = ref<boolean>(props.show);
+    const openPopOver = () => {
+      isActive.value = !isActive.value;
+    };
+    const closePopOver = (e: MouseEvent) => {
+      if (isActive.value) {
+        isActive.value = false;
+      }
+      e.stopImmediatePropagation();
+    };
+
+    //isActive value set to false, when the props.show value change triggered
+    watch(
+        () => props.show,
+        (val) => {
+          isActive.value = false;
+        }
+    );
+
+    return {
+      isActive,
+      openPopOver,
+      closePopOver,
+    };
+  },
+});
+</script>
+
+<style src="./pop-over.scss" lang="scss" scoped></style>
