2dcb47c00e4ff0f000ff7de473bfa0a9b8ec6ba0
Thilina Sumithrarachchi <thilinasan166@gmail.com>
2022-09-23 19:00:36 +0530

    RMP-909 update tabs component to preseve tab state

--- components/src/core/components/Tabs/Tabs.vue
+++ components/src/core/components/Tabs/Tabs.vue
@@ -7,7 +7,7 @@
         @click="onClick($event, tab)"
         @focus="onFocus($event, tab)"
         @blur="onBlur($event, tab)"
-        :class="{active: currentTabId == tab.id}"
+        :class="{active: selectedTabId == tab.id}"
         @keyup.enter="onClick($event, tab)"
         :id="tab.id"
       >
@@ -17,46 +17,52 @@
       >
       <div
         class="selected-tab-indicator"
-        :class="{active: currentTabId == tab.id}"
+        :class="{active: selectedTabId == tab.id}"
       ></div>
     </li>
   </ul>
   <div class="tab-content">
-    <KeepAlive>
-      <slot :name="currentTabId"></slot>
-    </KeepAlive>
+    <slot></slot>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from 'vue';
+import {defineComponent, ref, provide} from 'vue';
 import {Tab} from './types';
 import Icon from '@orangehrm/oxd/core/components/Icon/Icon.vue';
 import translateMixin from '../../../mixins/translate';
 
 export default defineComponent({
   name: 'oxd-tabs',
-  props: {
-    tabs: {
-      type: Array as () => Array<Tab>,
-      required: true,
-    },
-  },
   components: {
     'oxd-icon': Icon,
   },
   mixins: [translateMixin],
-  setup: function(props) {
-    const currentTabId = ref<string>('');
+  setup: function(props, {slots}) {
+    const tabs = ref(
+      slots.default
+        ? slots
+            .default()
+            .filter(value => value.props?.tab)
+            .map(value => {
+              return {
+                id: value.props?.tab.id,
+                title: value.props?.tab.title,
+                icon: value.props?.tab.icon,
+              };
+            })
+        : [],
+    );
+    const selectedTabId = ref(((tabs as unknown) as any)?.value[0].id);
+    provide('selectedTabId', selectedTabId);
 
     const setCurrentTabId = (tab: Tab): void => {
-      currentTabId.value = tab.id;
+      selectedTabId.value = tab.id;
     };
-
-    setCurrentTabId(props.tabs[0]);
     return {
-      currentTabId,
       setCurrentTabId,
+      selectedTabId,
+      tabs,
     };
   },
   emits: ['click', 'focus', 'blur', 'change'],
