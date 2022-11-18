3f92974bd0b17799dddf050db19744dbd7537c4c
Thilina Sumithrarachchi <thilinasan166@gmail.com>
2022-11-18 16:30:33 +0530

    charts adding for ent branch

--- components/src/core/components/Chart/chart.ts
+++ components/src/core/components/Chart/chart.ts
@@ -1,22 +1,3 @@
-/*
- * This file is part of OrangeHRM Inc
- *
- * Copyright (C) 2020 onwards OrangeHRM Inc
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see  http://www.gnu.org/licenses
- */
-
 import {
   Chart,
   Title,
@@ -29,7 +10,6 @@ import {
   BarController,
   PieController,
 } from 'chart.js';
-import {OxdChartLabels} from './plugins/labelPlugin';
 import {OxdBarChartFill} from './plugins/barfillPlugin';
 import {OxdPieChartTooltip} from './plugins/tooltipPlugin';
 
@@ -39,7 +19,6 @@ Chart.register(
   Tooltip,
   ArcElement,
   PieController,
-  OxdChartLabels,
   OxdPieChartTooltip,
 );
 
--- storybook/stories/core/components/Charts/PieChart.stories.js
+++ storybook/stories/core/components/Charts/PieChart.stories.js
@@ -1,24 +1,4 @@
-/*
- * This file is part of OrangeHRM Inc
- *
- * Copyright (C) 2020 onwards OrangeHRM Inc
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see  http://www.gnu.org/licenses
- */
-
 import PieChart from '@orangehrm/oxd/core/components/Chart/PieChart';
-// import {generateRandomColor} from '../../../../../components/src/utils/color';
 
 export default {
   title: 'Example/Charts/Pie',
@@ -39,12 +19,39 @@ Default.args = {
   height: '400px',
   responsive: false,
   title: 'Pie Chart',
+  cutout: '50%',
+  legend: {
+    display: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
   data: [
-    {value: 55, label: 'Cheese', color: 'red'},
-    {value: 20, label: 'Ham', color: 'blue'},
-    {value: 80, label: 'Bacon', color: 'green'},
-    {value: 10, label: 'Taco', color: 'purple'},
-    {value: 300, label: 'Butter', color: 'orange'},
+    {
+      value: 55,
+      label: 'Cheese',
+      color: 'red',
+    },
+    {
+      value: 20,
+      label: 'Ham',
+      color: 'blue',
+    },
+    {
+      value: 80,
+      label: 'Bacon',
+      color: 'green',
+    },
+    {
+      value: 10,
+      label: 'Taco',
+      color: 'purple',
+    },
+    {
+      value: 300,
+      label: 'Butter',
+      color: 'orange',
+    },
   ],
 };
 
