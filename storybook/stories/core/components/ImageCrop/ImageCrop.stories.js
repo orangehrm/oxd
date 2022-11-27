d1c49de56f3d7ec9f0b435f81a9293e7253026e9
Ruchira Amarasinghe <rtamarasinghe@gmail.com>
2022-11-27 23:58:15 +0530

    Add props resizeResult, imageFormat and imageQuality to ImageCropper.

--- storybook/stories/core/components/ImageCrop/ImageCrop.stories.js
+++ storybook/stories/core/components/ImageCrop/ImageCrop.stories.js
@@ -1,4 +1,5 @@
 import ImageCropper from '@orangehrm/oxd/core/components/ImageCropper/ImageCropper';
+import {ref} from "vue";
 
 export default {
   title: 'Inputs/ImageCropper',
@@ -17,10 +18,22 @@ export default {
       },
     },
     stencilHeight: {
-      control: {type: 'boolean'},
+      control: {type: 'text'},
       table: {
         type: {summary: 'px height of stencil'},
       },
+    },
+    imageFormat: {
+      control: {type: 'select', options: ['image/jpeg', 'image/gif', 'image/png', 'image/avif']},
+      table: {
+        type: {summary: 'image file type'},
+      }
+    },
+    imageQuality: {
+      control: {type: 'text'},
+      table: {
+        type: {summary: 'Image quality: floating point number between 0 and 1. Defaults to 0.9'},
+      }
     },
     change: {
       control: {type: 'function'},
@@ -33,16 +46,41 @@ export default {
 
 const Template = (args) => ({
   setup() {
-    return {args};
+    const croppedImage = ref('');
+    const imageSize = ref('');
+    const croppedImageWidth = ref('');
+    const croppedImageHeight = ref('');
+
+    const onImageCropped = (e) => {
+      croppedImage.value = e;
+      const base64 = e.split(",")[1];
+      imageSize.value = (atob(base64).length / 1024).toFixed(2);
+
+      const img = new Image();
+      img.onload = function() {
+        croppedImageWidth.value = this.width;
+        croppedImageHeight.value = this.height;
+      }
+      img.src = e;
+    };
+
+    const stencilWidth = args.stencilWidth ?? '280';
+    const stencilHeight = args.stencilHeight ?? '280';
+    return {args, croppedImage, onImageCropped, imageSize, stencilWidth, stencilHeight, croppedImageWidth, croppedImageHeight};
   },
   components: {'oxd-image-crop': ImageCropper},
-  template: '<oxd-image-crop v-bind="args" />',
+  template: '<div style="width:900px;height:600px;position:absolute"><oxd-image-crop v-bind="args" @change="onImageCropped"/></div>' +
+      '<div style="position:absolute;left:950px;top:0;display:flex; flex-direction: column">' +
+      '<span style="font-weight:bold;">Cropped Image Result (Not part of component)</span><img :src="croppedImage" alt="crop result" style="" :width="stencilWidth" :height="stencilHeight"/>' +
+      '<span>Image Size: {{ imageSize }} kb</span><span>Width: {{ croppedImageWidth }}, Height: {{ croppedImageHeight }}</span></div>',
 });
 
 export const Default = Template.bind({});
 Default.args = {
   image:
-    'https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&auto=format&fit=crop&w=991&q=80',
+    'https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&auto=format&fit=crop&w=2048&q=80',
+  imageFormat: 'image/jpeg',
+  imageQuality: '0.9',
 };
 
 export const CustomStencil = Template.bind({});
@@ -50,5 +88,8 @@ CustomStencil.args = {
   stencilHeight: '500',
   stencilWidth: '500',
   image:
-    'https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&auto=format&fit=crop&w=991&q=80',
+    'https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&fit=crop&w=2048&q=80',
+  imageFormat: 'image/jpeg',
+  imageQuality: 0.9,
 };
+
