d5e39a6a953513de221b1eb40e5a9c15969d9a3e
shachiniM <shachini@orangehrmlive.com>
2024-03-05 16:17:58 +0530

    Add oxd-move-up & oxd-move-down icons

--- components/src/core/components/Icon/icons.ts
+++ components/src/core/components/Icon/icons.ts
@@ -1448,10 +1448,7 @@ export const oxdAcknowledgement: icon = {
 
 export const oxdSpeechBubble: icon = {
   name: 'oxd-speech-bubble',
-  value: `<?xml version="1.0" encoding="iso-8859-1"?>
-  <!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-  <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+  value: `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
      width="28.621px" height="28.621px" viewBox="0 0 28.621 28.621" style="enable-background:new 0 0 28.621 28.621;"
      xml:space="preserve" style="fill:currentColor">
   <g>
@@ -1490,6 +1487,20 @@ export const oxdSpeechBubble: icon = {
   </g>
   <g>
   </g>
+  </svg>`,
+};
+
+export const oxdMoveUp: icon = {
+  name: 'oxd-move-up',
+  value: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M11.2929 4.29289C11.6834 3.90237 12.3166 3.90237 12.7071 4.29289L18.7071 10.2929C19.0976 10.6834 19.0976 11.3166 18.7071 11.7071C18.3166 12.0976 17.6834 12.0976 17.2929 11.7071L13 7.41421V19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19V7.41421L6.70711 11.7071C6.31658 12.0976 5.68342 12.0976 5.29289 11.7071C4.90237 11.3166 4.90237 10.6834 5.29289 10.2929L11.2929 4.29289Z" style="fill:currentColor"/>
+  </svg>`,
+};
+
+export const oxdMoveDown: icon = {
+  name: 'oxd-move-down',
+  value: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C12.5523 4 13 4.44772 13 5V16.5858L17.2929 12.2929C17.6834 11.9024 18.3166 11.9024 18.7071 12.2929C19.0976 12.6834 19.0976 13.3166 18.7071 13.7071L12.7071 19.7071C12.5196 19.8946 12.2652 20 12 20C11.7348 20 11.4804 19.8946 11.2929 19.7071L5.29289 13.7071C4.90237 13.3166 4.90237 12.6834 5.29289 12.2929C5.68342 11.9024 6.31658 11.9024 6.70711 12.2929L11 16.5858V5C11 4.44772 11.4477 4 12 4Z" style="fill:currentColor" />
   </svg>`,
 };
 
@@ -1657,6 +1668,8 @@ const icons: Icons = {
   'oxd-docs-check': oxdDocsCheck,
   'oxd-acknowledgement': oxdAcknowledgement,
   'oxd-speech-bubble': oxdSpeechBubble,
+  'oxd-move-up': oxdMoveUp,
+  'oxd-move-down': oxdMoveDown,
 };
 
 export default icons;
