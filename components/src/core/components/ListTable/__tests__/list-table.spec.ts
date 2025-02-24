5b4bec5a195794c00574e8ab5b949f5b7292b343
Chamara Abesinghe <chamara@orangehrm.com>
2025-02-24 19:31:35 +0530

    SF-378: Add unit tests for single, multiple, and select all scenarios of list table

--- components/src/core/components/ListTable/__tests__/list-table.spec.ts
+++ components/src/core/components/ListTable/__tests__/list-table.spec.ts
@@ -10,11 +10,10 @@ const DUMMY_DATA = {
   items: [
     {col1: 'Data 1', col2: 'Data 2'},
     {col1: 'Data 2', col2: 'Data 2'},
+    {col1: 'Data 3', col2: 'Data 3'},
   ],
   checkedItems: [2, 0],
 };
-
-jest.mock('../../../../utils/emitter');
 
 describe('ListTable > ListTable.vue', () => {
   it('renders OXD ListTable > ListTable', () => {
@@ -149,5 +148,111 @@ describe('ListTable > ListTable.vue', () => {
     const checkbox = tableHeader.find('input[type="checkbox"]');
 
     expect(checkbox.attributes('disabled')).toEqual('');
+  });
+
+  it('should emit event when single row is selected', async () => {
+    const wrapper = mount(ListTable, {
+      props: {
+        selectable: true,
+        items: DUMMY_DATA.items,
+        headers: DUMMY_DATA.headers,
+        selected: [],
+      },
+    });
+
+    const checkboxes = wrapper.findAll(
+      '.oxd-table-body .checkbox-cell input[type="checkbox"]',
+    );
+    await checkboxes[0].setValue(true);
+    await checkboxes[0].trigger('change');
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted('update:selected')).toBeTruthy();
+    expect(wrapper.emitted('update:selected')![0]).toEqual([[0]]);
+  });
+
+  it('should emit event when multiple rows are selected', async () => {
+    const wrapper = mount(ListTable, {
+      props: {
+        selectable: true,
+        items: DUMMY_DATA.items,
+        headers: DUMMY_DATA.headers,
+        selected: [0],
+      },
+    });
+
+    const checkboxes = wrapper.findAll(
+      '.oxd-table-body .checkbox-cell input[type="checkbox"]',
+    );
+    await checkboxes[2].setValue(true);
+    await checkboxes[2].trigger('change');
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted('update:selected')).toBeTruthy();
+    expect(wrapper.emitted('update:selected')![0]).toEqual([[0, 2]]);
+  });
+
+  it('should emit event when row is unselected', async () => {
+    const wrapper = mount(ListTable, {
+      props: {
+        selectable: true,
+        items: DUMMY_DATA.items,
+        headers: DUMMY_DATA.headers,
+        selected: [0, 2],
+      },
+    });
+
+    const checkboxes = wrapper.findAll(
+      '.oxd-table-body .checkbox-cell input[type="checkbox"]',
+    );
+    await checkboxes[0].setValue(false);
+    await checkboxes[0].trigger('change');
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted('update:selected')).toBeTruthy();
+    expect(wrapper.emitted('update:selected')![0]).toEqual([[2]]);
+  });
+
+  it('should emit event when select all is clicked', async () => {
+    const wrapper = mount(ListTable, {
+      props: {
+        selectable: true,
+        items: DUMMY_DATA.items,
+        headers: DUMMY_DATA.headers,
+        selected: [],
+      },
+    });
+
+    const selectAllCheckbox = wrapper.find(
+      '.oxd-table-header .checkbox-cell input[type="checkbox"]',
+    );
+    await selectAllCheckbox.setValue(true);
+    await selectAllCheckbox.trigger('change');
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted('update:selected')).toBeTruthy();
+    expect(wrapper.emitted('update:selected')![0]).toEqual([[0]]);
+  });
+
+  it('should emit event when unselect all is clicked', async () => {
+    const wrapper = mount(ListTable, {
+      props: {
+        selectable: true,
+        items: DUMMY_DATA.items,
+        headers: DUMMY_DATA.headers,
+        selected: [0, 1, 2],
+      },
+    });
+
+    const selectAllCheckbox = wrapper.find(
+      '.oxd-table-header .checkbox-cell input[type="checkbox"]',
+    );
+    await selectAllCheckbox.setValue(false);
+    await selectAllCheckbox.trigger('change');
+    await wrapper.vm.$nextTick();
+
+    // Remaining rows are kept in selection
+    expect(wrapper.emitted('update:selected')).toBeTruthy();
+    expect(wrapper.emitted('update:selected')![0]).toEqual([[1, 2]]);
   });
 });
