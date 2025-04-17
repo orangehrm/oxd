0488e16c8507c454f4fcafff4c2627c5098aa786
Aruna Tebel <arunautebel@gmail.com>
2025-04-17 13:06:50 +0000

    ROS-1: Add roster menu item icon to the OXD library

--- components/src/core/components/Icon/icons.ts
+++ components/src/core/components/Icon/icons.ts
@@ -2241,6 +2241,13 @@ export const oxdAIContent: icon = {
 </svg>`,
 };
 
+export const oxdRosterSchedule: icon = {
+  name: 'oxd-ai-content',
+  value: `<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17 11C14.2386 11 12 13.2386 12 16C12 18.7614 14.2386 21 17 21C19.7614 21 22 18.7614 22 16C22 13.2386 19.7614 11 17 11ZM17 11V9M2 9V15.8C2 16.9201 2 17.4802 2.21799 17.908C2.40973 18.2843 2.71569 18.5903 3.09202 18.782C3.51984 19 4.0799 19 5.2 19H13M2 9V8.2C2 7.0799 2 6.51984 2.21799 6.09202C2.40973 5.71569 2.71569 5.40973 3.09202 5.21799C3.51984 5 4.0799 5 5.2 5H13.8C14.9201 5 15.4802 5 15.908 5.21799C16.2843 5.40973 16.5903 5.71569 16.782 6.09202C17 6.51984 17 7.0799 17 8.2V9M2 9H17M5 3V5M14 3V5M15 16H17M17 16H19M17 16V14M17 16V18" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`,
+};
+
 const icons: Icons = {
   'oxd-likes': oxdLikes,
   'oxd-birthday': oxdBirthday,
@@ -2484,6 +2491,7 @@ const icons: Icons = {
   'oxd-detailed-report': oxdDetailedReport,
   'oxd-overnight-shift': oxdOvernightShift,
   'oxd-ai-content': oxdAIContent,
+  'oxd-roster-schedule': oxdRosterSchedule,
 };
 
 export default icons;
