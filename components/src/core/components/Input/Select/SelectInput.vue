2439ddb23cab956a3db9450e858b11ce504dd1e3
infas <infas@orangehrm.com>
2025-07-15 12:17:46 +0530

    Fix comments

--- components/src/core/components/Input/Select/SelectInput.vue
+++ components/src/core/components/Input/Select/SelectInput.vue
@@ -21,7 +21,7 @@
     </oxd-select-text>
 
     <oxd-select-dropdown
-      v-dropdown-direction="!forceDropdownPosition"
+      v-dropdown-direction="forceDropdownPosition === false"
       v-if="dropdownOpen"
       :class="dropdownClasses"
       :loading="isLoading"
