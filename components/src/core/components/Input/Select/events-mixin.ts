b307d09639d6ba63bb0d702f7a1b12d0495ccccc
dhanu <dhanu@orangehrm.com>
2025-10-16 17:31:41 +0530

    change scrollToView behaviour as instant

--- components/src/core/components/Input/Select/events-mixin.ts
+++ components/src/core/components/Input/Select/events-mixin.ts
@@ -78,7 +78,7 @@ export const eventsMixin = defineComponent({
     },
     scrollToView(elm: HTMLElement) {
       elm.scrollIntoView({
-        behavior: 'smooth',
+        behavior: 'instant',
         block: 'start',
         inline: 'start',
       });
