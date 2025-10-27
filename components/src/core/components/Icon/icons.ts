85c78978bb83adfe85ad56cf67a5b9bc4820a353
shachini <shachini@orangehrm.con>
2025-10-27 22:15:57 +0530

    Add oxd-remove and oxd-ok icons

--- components/src/core/components/Icon/icons.ts
+++ components/src/core/components/Icon/icons.ts
@@ -2589,6 +2589,20 @@ export const oxdMic: icon = {
 </svg>`,
 };
 
+export const oxdRemove: icon = {
+  name: 'oxd-remove',
+  value: `<svg width="100%" height="100%" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.64645 4.64645C4.84171 4.45118 5.15829 4.45118 5.35355 4.64645L8 7.29289L10.6464 4.64645C10.8417 4.45118 11.1583 4.45118 11.3536 4.64645C11.5488 4.84171 11.5488 5.15829 11.3536 5.35355L8.70711 8L11.3536 10.6464C11.5488 10.8417 11.5488 11.1583 11.3536 11.3536C11.1583 11.5488 10.8417 11.5488 10.6464 11.3536L8 8.70711L5.35355 11.3536C5.15829 11.5488 4.84171 11.5488 4.64645 11.3536C4.45118 11.1583 4.45118 10.8417 4.64645 10.6464L7.29289 8L4.64645 5.35355C4.45118 5.15829 4.45118 4.84171 4.64645 4.64645Z" style="fill:currentColor"/>
+</svg>`,
+};
+
+export const oxdOk: icon = {
+  name: 'oxd-ok',
+  value: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10.9696 4.96967C11.2625 4.67678 11.7374 4.67678 12.0303 4.96967C12.3196 5.25897 12.3231 5.72582 12.0409 6.01947L8.04873 11.0097C8.04297 11.0169 8.03682 11.0238 8.03029 11.0303C7.7374 11.3232 7.26253 11.3232 6.96963 11.0303L4.32319 8.38388C4.03029 8.09099 4.03029 7.61612 4.32319 7.32322C4.61608 7.03033 5.09095 7.03033 5.38385 7.32322L7.47737 9.41674L10.9497 4.9921C10.9559 4.98424 10.9626 4.97674 10.9696 4.96967Z" style="fill:currentColor"/>
+</svg>`,
+};
+
 const icons: Icons = {
   'oxd-likes': oxdLikes,
   'oxd-birthday': oxdBirthday,
@@ -2869,6 +2883,8 @@ const icons: Icons = {
   'oxd-delete': oxdDelete,
   'oxd-dollar-symbol': oxdDollarIcon,
   'oxd-mic': oxdMic,
+  'oxd-remove': oxdRemove,
+  'oxd-ok': oxdOk,
 };
 
 export default icons;
